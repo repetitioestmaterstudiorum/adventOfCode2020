81df7192dc216ba7cddb422e896fb95d90677ffc
sunyamare <sunyamare@gmail.com>
2020-12-15 22:29:42 +0100

    more speed optimization

--- day-15/index.js
+++ day-15/index.js
@@ -1,7 +1,8 @@
 /* read actual or sample data */
 const fs = require("fs")
+const { start } = require("repl")
 
-const actualDataPath = `${process.cwd()}/day-15/actuals.txt`
+const actualDataPath = `${process.cwd()}/day-15/actual.txt`
 const sampleDataPath = `${process.cwd()}/day-15/sample.txt`
 
 function readFilePathSync(filePath) {
@@ -18,20 +19,20 @@ function readFilePathSync(filePath) {
 
 const data = readFilePathSync(actualDataPath) || readFilePathSync(sampleDataPath)
 
-const startSequency = data.split(",").map(x => Number(x))
+const startSequence = data.split(",").map(x => Number(x))
+const startSequenceMinusLast = startSequence.slice(0, startSequence.length - 1)
+let lastNumber = startSequence.pop()
 
 /* PART 1 */
 function logSpokenNumberAtPosition(position) {
-	let spokenNumbers = startSequency
-	for (let i = startSequency.length + 1; i <= position; i++) {
-		const length = spokenNumbers.length
-		const lastNumber = spokenNumbers[length - 1]
-		const lastInstanceOfLastNumber = spokenNumbers.slice(0, length - 1).lastIndexOf(lastNumber)
-		// if number not occurred yet, add 0, else find distance and add it
+	let spokenNumbers = startSequenceMinusLast
+	for (let i = spokenNumbers.length + 1; i <= position; i++) {
+		const lastInstanceOfLastNumber = spokenNumbers.lastIndexOf(lastNumber)
+		spokenNumbers.push(lastNumber)
 		if (lastInstanceOfLastNumber === -1) {
-			spokenNumbers.push(0)
+			lastNumber = 0
 		} else {
-			spokenNumbers.push(length - 1 - lastInstanceOfLastNumber)
+			lastNumber = spokenNumbers.length - 1 - lastInstanceOfLastNumber
 		}
 		if (i === position) console.log("result: ", spokenNumbers.pop())
 	}
